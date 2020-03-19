0c42cd52c6f9fe5319131647c8a9d1dfab11a191
Sam Barker <samuel.e.barker@googlemail.com>
2020-03-19 12:45:31 +0000

    Automated deploy of exercises

---
+++ 19-adding-to-arrays-exercise/index.js
@@ -0,0 +1,47 @@
+/**
+ * create an array "animals" with "cat"
+ */
+
+/**
+ * Exercise 1
+ *
+ * add a "dog" to the end of your 'animals'
+ * 
+ * PS: console.log() the result to see the difference
+ */
+
+
+/**
+ * Exercise 2
+ *
+ * add a "rabbit" to the very beginning of your 'animals'
+ * 
+ * PS: console.log() the result to see the difference
+ */
+
+
+/**
+ * Exercise 3
+ *
+ * add a "puma" to position 2 in "animals"
+ * 
+ * PS: console.log() the result to see the difference
+ */
+
+
+/**
+ * create an array "wildAnimals" with "lion"
+ */
+
+/**
+ * create an array "wildAnimals" with "giraffe" and "elephant"
+ */
+
+/**
+ * Exercise 4
+ *
+ * combine wildAnimals and moreWildAnimals into one array
+ * and save it into variable combinedWildAnimals
+ * 
+ * PS: console.log() the result to see the difference
+ */
