53e50a69f0edaa06750a2d9225f24ad7bed86fde
benny poliak <benderu.m@gmail.com>
2020-03-22 17:43:14 +0000

    21 objects done

--- 21-creating-objects-exercise/index.js
+++ 21-creating-objects-exercise/index.js
@@ -7,6 +7,12 @@
  * PS: use console.log() to see the result
  */
 
+const user = {
+    firstName: "benny",
+    lastName: "polak",
+    age: 35
+}
+console.log(user);
 /**
  * Exercise 2
  *
@@ -15,7 +21,21 @@
  *
  * PS: use console.log() to see the result
  */
-
+const allTypes = {
+    string: "jumble",
+    number: 23,
+    array: [1, 2, 4, 5, 7],
+    object: {
+        oneThing: "good thing",
+        secondThing: "bad thing",
+        thirdThing: "random thing"
+    },
+    boolean: false,
+    functor: function here() {
+        console.log("bring me the money")
+    }
+};
+console.log(allTypes);
 /**
  * Exercise 3
  *
@@ -25,3 +45,17 @@
  *
  * PS: use console.log() to see the result
  */
+
+function createMovieObject(name, rating, ticketPrice) {
+    const movie = {
+        name: name,
+        rating: rating,
+        ticketPrice: ticketPrice
+
+    };
+
+    console.log(movie);
+    return movie;
+}
+
+createMovieObject("oralndo", 9.6, "10USD");
