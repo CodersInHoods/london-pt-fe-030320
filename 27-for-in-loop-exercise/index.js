0e09426f963aa6744b51d4ddb905b198e3e56c01
Sam Barker <samuel.e.barker@googlemail.com>
2020-03-22 08:45:45 +0000

    Automated deploy of exercises

---
+++ 27-for-in-loop-exercise/index.js
@@ -0,0 +1,25 @@
+/**
+ * Exercise 1
+ * create a function {logProperties} which will
+ * take object as a param and console log each property in a format
+ * "Property name: {propertyName}"
+ */
+
+/**
+ * Exercise 2
+ * Create a function {getAllValues} that takes an object
+ * and returns an array of all values.
+ */
+
+/**
+ * Exercise 3
+ * Create a function {getAllValuesAsString} that takes an object
+ * and returns all values as one string with space separation.
+ */
+
+/**
+ * Exercise 4
+ * Create a function {propertyValueString} that takes object as a param
+ * and log property and values as string in a next format:
+ * 'PROPERTY: VALUE'
+ */
