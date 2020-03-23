b948abfa4c4551bd9a8a0b9c51effcc0b6ac6504
benny poliak <benderu.m@gmail.com>
2020-03-23 20:36:15 +0000

    23 is ready

--- 23-adding-object-properties/index.js
+++ 23-adding-object-properties/index.js
@@ -9,16 +9,26 @@ const user = {};
  * 
  * add property "firstName" to "user" and assign any name 
  */
-
+user.firstName = "benjamin";
+console.log(user);
 /**
  * Exercise 2
  * 
  * add property "lastName" to "user" and assign any last name 
  */
-
+user.lastName = "pol";
+console.log(user);
 /**
  * Exercise 3
  * create a function "addANewProperty" which takes
  * 2 params, first - property name and second - property value
  * and set a new property for 'user' object
  */
+function addANewProperty(propertyName, propertyValue) {
+    user[propertyName] = propertyValue;
+    return
+}
+
+addANewProperty("height", 23.3)
+
+console.log(user);
