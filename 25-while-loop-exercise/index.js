8c686524d0261a7b9fd7daf2c32ab62f864d58b2
benny poliak <benderu.m@gmail.com>
2020-03-24 16:58:16 +0000

    add 25

--- 25-while-loop-exercise/index.js
+++ 25-while-loop-exercise/index.js
@@ -10,17 +10,36 @@ let count = 10; // DO NOT TOUCH THIS LINE :)
  */
 
 // ==========================
+function countdown() {
+    i = 10;
+    while (i != 0) {
+        console.log(`Remaining: ${i}`);
+        i--;
+    }
+    console.log("Go!")
+}
 
+countdown(15);
 /**
  * Exercise 2
  *
- * create a function {countdownWithParam} which should
+ * create a function {} which should
  * be the same as {countdown} the only difference is
  * that it takes any number as a param and count from that point
  */
+function countdownWithParam(Param) {
+    i = Param;
+    while (i != 0) {
+        console.log(`Remaining: ${i}`);
+        i--;
+    }
+    console.log("Go!")
+}
 
+countdownWithParam(15);
 // ==========================
 let ruler = [];
+
 /**
  * Exercise 3
  * create a function {rulerConstructor} and by using while loop
@@ -29,7 +48,22 @@ let ruler = [];
  * The result of calling {rulerConstructor} is that {ruler}
  * will be [0,10,20,30,40,50,60,70,80,90,100]
  */
+function rulerConstructor() {
+    i = 0;
+    counter = 0;
+    ruler.push(0);
+    while (ruler[i] < 100) {
+        i++;
+        console.log(i);
+        counter = counter + 10;
+        ruler.push(counter);
+        console.log(ruler[i])
+    }
 
+    return ruler;
+}
+rulerConstructor();
+console.log(ruler);
 // ==========================
 
 /**
@@ -39,3 +73,10 @@ let ruler = [];
  * "Neighbour" to the array until you have at least 5 guests.
  * Without using an if statement!
  */
+function partyPadding(nameArray) {
+    while (nameArray.length < 5) {
+        nameArray.push("Neighbour");
+    }
+    console.log(nameArray);
+}
+partyPadding(["ben", "sammy", "elton"]);
