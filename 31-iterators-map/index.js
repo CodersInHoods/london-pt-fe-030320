5aa10442ab143c5b0aabf62676b4a66225dcd8a6
benny poliak <benderu.m@gmail.com>
2020-03-30 22:19:45 +0100

    31!!! done!

--- 31-iterators-map/index.js
+++ 31-iterators-map/index.js
@@ -9,6 +9,18 @@
  * array of numbers, convert all negatives to positives and return
  * a new array.
  */
+array = [1, 4, -6, 8, -9, 4, -2];
+
+const convertToPositive = array =>
+    array.map(item => {
+        if (item < 0) {
+            item = item * -1;
+        }
+        return item;
+    })
+
+console.log(convertToPositive(array));
+
 
 /**
  * Exercise 2
@@ -22,6 +34,20 @@
  *   {name: 'John', age: 25}
  * ]) will return ['Alex', 'John']
  */
+const users = [
+    { name: 'Alex', age: 26 },
+    { name: 'John', age: 25 }
+]
+
+const getAllNames = users =>
+    users.map(elem => elem.name);
+
+console.log(getAllNames([
+    { name: 'Alex', age: 26 },
+    { name: 'John', age: 25 }
+]));
+
+
 
 /**
  * Exercise 3
@@ -29,6 +55,13 @@
  * create a function {greetAll} that takes array of names as a parameter
  * and return a new array of greetings "Hi, {name}!"
  */
+const names = [`niv`, `ami`, `shimon`];
+
+const greetAll = names =>
+    names.map(name => `Hi, ${name}!`);
+
+
+console.log(greetAll([`niv`, `ami`, `shimon`]));
 
 /**
  * Exercise 4
@@ -37,3 +70,15 @@
  * and multiply all numbers inside array by 5 and return the whole array
  * with multiplied numbers
  */
+
+const multiplyAllNumbers = array =>
+    array.map(elem => {
+        if (typeof elem === "number") {
+            elem = elem * 5;
+            return elem;
+        } else {
+            return elem;
+        }
+    })
+
+console.log(multiplyAllNumbers(["Alex", 6, "John", 7, "Mimi", 4]));
