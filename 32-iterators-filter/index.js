54480bb591813743e74bb09bbf2fd43038ace142
benny poliak <benderu.m@gmail.com>
2020-03-30 23:21:53 +0100

    32 and away

--- 32-iterators-filter/index.js
+++ 32-iterators-filter/index.js
@@ -5,6 +5,10 @@
  * as a parameter and return an array but without strings
  */
 
+const noStrings = array =>
+    array.filter(item => typeof item !== "string");
+
+console.log(noStrings(["noah", "amy", "shlomo", 2, 6, 2, "david"]));
 /**
  * Exercise 2
  *
@@ -13,6 +17,10 @@
  * every other type
  */
 
+const justStrings = array =>
+    array.filter(item => typeof item === "string");
+
+console.log(justStrings(["noah", "amy", "shlomo", 2, 6, 2, "david"]));
 /**
  * Exercise 3
  *
@@ -20,7 +28,10 @@
  * an array as a parameter and return an array of strings
  * that are longer than 5 characters
  */
+const justStringsLongerThan5 = array =>
+    array.filter(item => item.length > 5);
 
+console.log(justStringsLongerThan5(["noah", "amy", "shlomo", 2, 6, 2, "david"]));
 /**
  * Exercise 4
  *
@@ -29,12 +40,21 @@
  * that are more than 13
  */
 
+const numbersMoreThan13 = array =>
+    array.filter(item => typeof "number").filter(item => item > 13);
+
+console.log(numbersMoreThan13(["noah", "amy", "shlomo", 2, 6, 23, "david"]));
+
 /**
  * Exercise 5
  *
  * create a function {objectsOnly} that takes
  * an array as a parameter and return an array of objects
  */
+const objectsOnly = array =>
+    array.filter(item => typeof item === "object");
+
+console.log(objectsOnly(["noah", "amy", { name: "shlomo" }, 2, 6, 23, "david"]));
 
 /**
  * Exercise 6
@@ -43,3 +63,15 @@
  * an array as a parameter and return an array of truthy values.
  * IMPORTANT: You still need to keep number 0.
  */
+const truthyOnly = array =>
+    array.filter(item => item || item === 0);
+
+console.log(truthyOnly([
+    "kitten",
+    {},
+    0,
+    false,
+    { name: "Alex" },
+    ["pet"],
+    26
+]));
