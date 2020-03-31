f89e97fc47c9a99d709305645165cafa3846b906
benny poliak <benderu.m@gmail.com>
2020-03-31 17:27:29 +0100

    35 working, not clea on error

--- 34-user-input-exercise/index.js
+++ 34-user-input-exercise/index.js
@@ -1,15 +1,21 @@
 // install and  import "readline-sync" npm package before you do exercises
+readlineSync = require('readline-sync');
 
 /**
  * Exercise 1
  *
  * ask user for a name and assign a response to variable {name}
  */
+let name = readlineSync.question("what is your name?")
+console.log(`Hi ${name}`);
 
 //===== DO NOT TOUCH THIS BLOCK =====
-console.log(`Hi ${name}!`);
+console.log(`
+        Hi $ { name }!`);
 console.log("=====================");
-console.log(`Let me do math for you!`);
+console.log(`
+        Let me do math
+        for you!`);
 console.log("=====================");
 //===================================
 
@@ -23,6 +29,16 @@ console.log("=====================");
  * NOTE: if the user will respond with wrong value, ask again, until
  * you get correct symbol
  */
+let selectedSymbol = readlineSync.question(`what math shall I do for you? choose a symbol (*,/,+,-)`);
+
+while (selectedSymbol !== `*` &&
+    selectedSymbol !== '/' &&
+    selectedSymbol !== "+" &&
+    selectedSymbol !== `-`) {
+    selectedSymbol = readlineSync.question(`what math shall I do for you? choose a symbol (*,/,+,-`);
+}
+
+
 
 /**
  * Exercise 3
@@ -33,6 +49,13 @@ console.log("=====================");
  * you get a number
  */
 
+let number1 = readlineSync.question("can you give me a number?");
+number1 = parseInt(number1, 10);
+while (typeof number1 !== "number") {
+    const number1 = readlineSync.question("can you give me a number?")
+    number1 = parseInt(number1, 10);
+}
+
 /**
  * Exercise 4
  *
@@ -41,6 +64,12 @@ console.log("=====================");
  * NOTE: if the user will respond with wrong value, ask again, until
  * you get a number
  */
+let number2 = readlineSync.question("can you give me another number?");
+number2 = parseInt(number2, 10);
+while (typeof number2 !== "number") {
+    const number2 = readlineSync.question("can you give me another number?")
+    number2 = parseInt(number2, 10);
+}
 
 /**
  * Exercise 5
@@ -50,6 +79,13 @@ console.log("=====================");
  *
  * show the result to the user
  */
+mathArray = ([number1, selectedSymbol, number2]);
+
+console.log(eval(mathArray.join(``)));
+const result = eval(mathArray.join(``))
+console.log(result);
 
 console.log("=====================");
-console.log(`Here you go, the result is ${result}`);
+console.log(`
+        Here you go, the result is ${result}
+        `);
