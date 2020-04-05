a4986cb392e5da45f27a34a9043da91c03e6bed8
benny poliak <benderu.m@gmail.com>
2020-04-05 12:38:12 +0100

    done, not so sure about 12 :)

--- 37-diy-array-iterators-exercise/index.js
+++ 37-diy-array-iterators-exercise/index.js
@@ -195,7 +195,6 @@ function reduce(array, callback, initialValue) {
     for (let i = 0; i < array.length; i++) {
         let element = array[i];
         cumValue = callback(cumValue, element, i);
-
     }
     return cumValue;
 }
--- 38-DOM-selecting-elements/index.js
+++ 38-DOM-selecting-elements/index.js
@@ -5,11 +5,17 @@
  * to variable {paragraphs}
  */
 
+let paragraphs = document.getElementsByTagName("p");
+paragraphs;
 /**
  * Exercise 2
  * select the first p tag with getElementsByTagName and assign it
  * to variable {firstParagraph}
  */
+
+let firstParagraph = document.getElementsByTagName("p")[0];
+firstParagraph;
+
 
 /**
  * Exercise 3
@@ -20,12 +26,23 @@
  * ATTENTION: check the type of {paragraphs} before doing forEach
  */
 
+function logAllParagraphs() {
+    for (let item of paragraphs) {
+        console.log(item);
+    }
+}
+
+
+
+
 /**
  * Exercise 4
  *
  * select all elements with class "text" with getElementsByClassName
  * and assign it to variable {textElements}
  */
+
+const textElements = document.getElementsByClassName("text");
 
 /**
  * Exercise 5
@@ -34,12 +51,17 @@
  * with getElementsByClassName and assign it to variable {greenText}
  */
 
+const greenText = document.getElementsByClassName("green text");
+
 /**
  * Exercise 6
  *
  * select element where id is 'textId-3'(use getElementById) and assign it
  * to variable {thirdText}
  */
+
+const thirdText = document.getElementById("kitten");
+
 
 /* ==================================================== */
 /*                  querySelector section               */
@@ -52,17 +74,23 @@
  * select the fourth "li" element and save it to {fourthListElement} variable
  */
 
+const fourthListElement = document.querySelector("li:nth-of-type(4)");
+
 /**
  * Exercise 8
  *
  * select all "li" elements and save it to {listElements} variable
  */
 
+const listElements = document.querySelectorAll("li");
+
 /**
  * Exercise 8
  *
  * select all "a" elements that are inside "ul" and assign it {listLinks}
  */
+
+const listLinks = document.querySelectorAll("ul > a");
 
 /**
  * Exercise 9
@@ -71,12 +99,15 @@
  * is "codersinhoods" and assign it {communityElements}
  */
 
+const communityElements = document.querySelectorAll("[data-community-name]");
+
 /**
  * Exercise 10
  *
  * select all elements with class "test" and assign it to a variable
  * {elementsForTesting}
  */
+const elementsForTesting = document.querySelectorAll(".test");
 
 /**
  * Exercise 11
@@ -85,9 +116,14 @@
  * {fifthTest}
  */
 
+const fifthTest = document.getElementById("test-5");
+
 /**
  * Exercise 12
  *
  * select all the elements which are siblings of an element
  * with the class "links" and assign it to a variable {nextElements}
  */
+const nextElements = []
+const bodyEl = document.querySelector(".links").nextElementSibling;
+nextElements.push(bodyEl);
