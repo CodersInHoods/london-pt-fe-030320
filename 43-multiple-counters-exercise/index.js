9b847de13546ec5e635bb2f71c4f21b586ba58aa
benny poliak <benderu.m@gmail.com>
2020-04-13 16:03:31 +0100

    all done but fails tests

--- 43-multiple-counters-exercise/index.js
+++ 43-multiple-counters-exercise/index.js
@@ -3,7 +3,14 @@ const timersContainerEl = document.querySelector(".timers");
 
 // USE THIS VARIABLE TO COUNT TIMERS
 let timersCount = 0;
+const timers = [];
 
+addCounterEl.addEventListener(`click`, () => {
+    if (timersCount < 5) {
+        createTimerEl(timersCount);
+        timers.push(timersCount);
+    }
+});
 /**
  * Exercise 1
  *
@@ -14,3 +21,68 @@ let timersCount = 0;
  * NOTE: each timer should have additional class with unique number(as you
  * can see in example - timer_$)
  */
+
+function createTimerEl(num) {
+    let seconds = 0;
+    let miliseconds = 0;
+
+    const stop = () => clearInterval(timers[num]);
+
+    const container = document.createElement("div");
+    container.className = `timer timer_${num}`;
+    timersContainerEl.append(container);
+    const remove = document.createElement("div");
+    remove.className = "remove";
+    container.appendChild(remove);
+    const h3 = document.createElement("h3");
+    h3.innerHTML = `${seconds} <span>${miliseconds}</span>`
+    container.appendChild(h3);
+    const buttons = document.createElement("div");
+    buttons.className = "btn";
+    container.appendChild(buttons);
+    const buttonReset = document.createElement("button");
+    buttonReset.className = "reset";
+    buttonReset.innerText = "Reset";
+    buttons.appendChild(buttonReset);
+    const buttonStop = document.createElement("button");
+    buttonStop.className = "stop";
+    buttonStop.innerText = "Stop";
+    buttons.appendChild(buttonStop);
+    timersCount++
+    const timer = document.querySelector(`.timer_${num} h3`);
+    const stopbtn = document.querySelector(`.timer_${num} .stop`);
+    const resetbtn = document.querySelector(`.timer_${num} .reset`);
+    const removebtn = document.querySelector(`.timer_${num} .remove`);
+
+    function addmili(inter) {
+        miliseconds += inter;
+        console.log(miliseconds);
+        timer.innerHTML = `${seconds} <span>${miliseconds /10 }</span>`
+        if (miliseconds === 950) {
+            seconds++;
+            miliseconds = 0;
+        }
+    }
+
+    const reset = () => {
+        seconds = 0;
+        miliseconds = 0;
+        timer.innerHTML = `${seconds} <span>${miliseconds /10 }</span>`;
+    }
+
+    timers[num] = setInterval(addmili, 10, 10);
+
+    removebtn.addEventListener("click", event => {
+        stop(num);
+        event.currentTarget.parentNode.remove();
+    })
+
+    resetbtn.addEventListener("click", reset);
+
+    stopbtn.addEventListener("click", () => {
+        stop(num);
+    })
+
+    return container;
+
+}
