422be8ff784ef38f0bb15a42747fd2e1bbec99b2
Sam Barker <samuel.e.barker@googlemail.com>
2020-05-12 21:55:55 +0100

    added innerText polyfill to 51, 52

--- 52-js-fetch-multiple-requests/tests/DOM.js
+++ 52-js-fetch-multiple-requests/tests/DOM.js
@@ -1,3 +1,11 @@
+Object.defineProperty(global.Element.prototype, "innerText", {
+	get() {
+	  return this.textContent;
+	},
+	set(str) {
+	  this.innerHTML = this.textContent = str;
+	},
+  });
 document.body.innerHTML = `
     <form action="#">
     	<input type="text" placeholder="Type member name" />
